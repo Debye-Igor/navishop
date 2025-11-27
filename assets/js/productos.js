bbebdadfe397844e7b5793af381a37349ebdf74d
Debye-Igor <codes.harol@gmail.com>
2025-11-27 17:26:06 -0300

    correcion rutas /

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -6,41 +6,41 @@ const productos = [
         nombre: "Árbol Navideño",
         precio:  69000,
         descripcion: "Árbol de 1.80m con luces incluidas",
-        imagen: "/assets/img/arbol.jpeg"
+        imagen: "assets/img/arbol.jpeg"
     },
     {
         id: 2,
         nombre: "Luces LED",
         precio: 14990,
         descripcion: "Set de 200 luces de colores",
-        imagen: "/assets/img/lucesled.jpeg"
+        imagen: "assets/img/lucesled.jpeg"
     },
     {
         id: 3,
         nombre: "Bolas Navideñas",
         precio: 9990,
         descripcion: "Pack de 24 bolas decorativas",
-        imagen: "https://images.unsplash.com/photo-1513297887119-d46091b24bfa?w=400"
+        imagen: "assets/img/bolas.webp"
     },
     {
         id: 4,
         nombre: "Corona",
         precio: 19990,
         descripcion: "Corona decorativa de 45cm",
-        imagen: "https://images.unsplash.com/photo-1576870851032-a8fed8cfe0ff?w=400"
+        imagen: "assets/img/corona.jpg"
     },
     {
         id: 5,
         nombre: "Viejito Pascuero",
         precio: 37990,
         descripcion: "Figura decorativa de 60cm",
-        imagen: "https://images.unsplash.com/photo-1606675994085-d0d05b384b82?w=400"
+        imagen: "assets/img/viejito.webp"
     },
     {
         id: 6,
         nombre: "Guirnalda",
         precio: 12990,
         descripcion: "Guirnalda artificial de 2 metros",
-        imagen: "https://images.unsplash.com/photo-1607606247874-f388c4c7a3d9?w=400"
+        imagen: "assets/img/guirnalda.jpeg"
     }
 ]
